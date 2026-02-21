d0716fa754ac83bb391b22fd303fdaab09feb4d8
Ivet13 <jazzjackrabbit13@gmail.com>
2026-02-21 21:40:14 +0100

    feat: Add HTML lang attribute and charset meta tag, then remove style and script stacks.

--- resources/views/components/admin/admin-layout.blade.php
+++ resources/views/components/admin/admin-layout.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ $title ?? 'Books Collection' }}</title>
+
+    <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @stack('styles')
 </head>
 
 <body class="layout">
@@ -42,7 +45,7 @@
     <footer>
         <p>© {{ date('Y') }} - Un Proyecto hecho con Laravel - &#x2764 </p>
     </footer>
-    @stack('scripts')
+
 </body>
 
 </html>
