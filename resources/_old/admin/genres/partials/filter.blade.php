804acc5b7a27392f92a00be96eafb30a7e39e0bd
Ivet13 <jazzjackrabbit13@gmail.com>
2026-02-21 21:50:53 +0100

    chore: Remove deprecated Blade templates from the resources/_old directory.

--- resources/_old/admin/genres/partials/filter.blade.php
+++
@@ -1,5 +0,0 @@
-<form class="js-filter-form" method="GET" action="{{ route('admin.genres.index') }}">
-    <button type="submit" title="Aplicar filtros"><x-icon.filter-menu /></button>
-    <input type="text" name="q" placeholder="Buscar género" value="{{ request('q') }}">
-    <button type="button" class="js-filter-reset">Reset</button>
-</form>
--- resources/_old/admin/publishers/partials/filter.blade.php
+++
@@ -1,5 +0,0 @@
-<form class="js-filter-form" method="GET" action="{{ route('admin.publishers.index') }}">
-    <button type="submit" title="Aplicar filtros"><x-icon.filter-menu /></button>
-    <input type="text" name="q" placeholder="Buscar editorial" value="{{ request('q') }}">
-    <button type="button" class="js-filter-reset">Reset</button>
-</form>
