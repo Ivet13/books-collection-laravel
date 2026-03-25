ed96790eb4dc159c4bf054ad89e083ede54da6bb
Ivet13 <jazzjackrabbit13@gmail.com>
2026-03-25 08:54:00 +0100

    chore: Remove author poster image display from admin tabs.

--- resources/views/components/admin/authors/tabs.blade.php
+++ resources/views/components/admin/authors/tabs.blade.php
@@ -29,22 +29,6 @@
                          <div style="width:100%;">
                              <label for="bio">Biography</label>
                              <textarea name="locale[en][bio]">{{ data_get($author, 'locale.en.bio', $author->bio ?? '') }}</textarea>
-                             <img data-original-filename="{{ $author->images[$locale]['lg']['poster'][0]['originalFilename'] }}"
-                                 srcset="
-        {{ route('image', ['entity' => 'authors', 'entityId' => $author->id, 'filename' => $author->images[$locale]['xs']['poster'][0]['filename']]) }} 480w,
-        {{ route('image', ['entity' => 'authors', 'entityId' => $author->id, 'filename' => $author->images[$locale]['sm']['poster'][0]['filename']]) }} 768w,
-        {{ route('image', ['entity' => 'authors', 'entityId' => $author->id, 'filename' => $author->images[$locale]['md']['poster'][0]['filename']]) }} 1024w,
-        {{ route('image', ['entity' => 'authors', 'entityId' => $author->id, 'filename' => $author->images[$locale]['lg']['poster'][0]['filename']]) }} 1440w
-      "
-                                 sizes="
-        (max-width: 480px) 480px,
-        (max-width: 768px) 768px,
-        (max-width: 1024px) 1024px,
-        1440px
-      "
-                                 src="{{ route('image', ['entity' => 'courses', 'entityId' => $course->id, 'filename' => $course->images[$locale]['lg']['poster'][0]['filename']]) }}"
-                                 alt="{{ $course->images[$locale]['lg']['poster'][0]['alt'] }}"
-                                 title="{{ $course->images[$locale]['lg']['poster'][0]['title'] }}">
                          </div>
 
                          <div id="ES" class="tabcontent" style="display: none;">
